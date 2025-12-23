34ee00eb61d5d20122705d13bed94fda56da9023
salmaysiman <salmaysiman@gamail.com>
2025-12-23 10:47:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+console.log(companyName);
+let it = 'Scuber';
+console.log(it);
+let mostProfitableNeighborhood = 'Chelsea';
+console.log(mostProfitableNeighborhood);
+let companyCeo = 'Susan Smith';
+console.log(companyCeo);    
